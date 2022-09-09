d957733f3593edc29c909f45a169033f55fc06ac
Ahmed Maz Dev <ahmedmazdev@gmail.com>
2022-09-09 16:16:19 +0100

    Fixed Api Route

--- src/pages/api/addPost.ts
+++ src/pages/api/addPost.ts
@@ -4,50 +4,40 @@ import { postIndex } from "../../lib/algolia";
 import { Post, PostAlgolia } from "../../types/Post";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json(req.body);
-  try {
-    if (!req.body.idToken) {
-      return res.status(400).send({
-        error: "no token",
-      });
-    }
-
-    admin
-      .auth()
-      .verifyIdToken(req.body.idToken)
-      .then(async (decodedToken) => {
-        const body = req.body as Post;
-
-        if (!body.title || !body.username) {
-          return res.status(400).send({
-            status: 400,
-            error: "Missing body",
-          });
-        }
-
-        const post = await postIndex.saveObject({
-          objectID: body.slug,
-          title: body.title,
-          userDisplayName: body.userDisplayName,
-          tags: body.tags,
-          createdAt: body.createdAt,
-          imageUrl: body.imageUrl,
-          slug: body.slug,
-        });
-
-        return res.status(200).json({
-          success: true,
-          data: {
-            post,
-          },
-        });
-      })
-      .catch((error) => {
-        return res.status(400).send({
-          error: "invalid token",
-        });
-      });
-  } catch (e) {
-    console.log("error", e);
+  if (!req.body.idToken) {
+    return res.status(400).send({
+      error: "no token",
+    });
   }
+
+  admin
+    .auth()
+    .verifyIdToken(req.body.idToken)
+    .then(async (decodedToken) => {
+      const body = req.body as Post;
+
+      if (!body.title || !body.username) {
+        return res.status(400).send({
+          status: 400,
+          error: "Missing body",
+        });
+      }
+
+      const post = await postIndex.saveObject({
+        objectID: body.slug,
+        ...body,
+      });
+
+      return res.status(200).json({
+        success: true,
+        data: {
+          post,
+        },
+      });
+    })
+    .catch((error) => {
+      return res.status(400).send({
+        error: "invalid token",
+      });
+    });
 };
